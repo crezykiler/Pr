92653409116f77db11dd1d1dfa91f5338caa4e6b
zaki setyawan <kiminsetyawan@gmail.com>
2024-10-04 19:27:45 +0700

    Sesudah modif

--- Pemilihan2Percobaan117.java
+++ Pemilihan2Percobaan117.java
@@ -9,9 +9,16 @@ public class Pemilihan2Percobaan117 {
         System.out.print("Masukkan tahun: ");
         tahun = input17.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
-            System.out.println("Tahun Kabisat");
+        if (tahun % 4 == 0) {
+            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            } else {
+                System.out.println("Tahun Kabisat");
+            }
         } else {
             System.out.println("Bukan Tahun Kabisat");
         }
