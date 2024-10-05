2b5a03683d66e98711d4853062d7dda220366fb3
zaki setyawan <kiminsetyawan@gmail.com>
2024-10-05 11:06:56 +0700

    sebelum modif

---
+++ Pemilihan2Percobaan317.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan317 {
+
+    public static void main(String[] args) {
+        Scanner input17 = new Scanner(System.in);
+
+        String kategori;
+        int penghasilan, gaji_bersih;
+        double pajak = 0;
+
+        System.out.print("Masukkan kategori : ");
+        kategori = input17.nextLine();
+        System.out.print("Masukkan besarnya penghasilan : ");
+        penghasilan = input17.nextInt();
+
+        if (kategori.equalsIgnoreCase("pekerja")) {
+            if (penghasilan <= 2000000)
+            pajak = 0.1;
+            else if (penghasilan <= 3000000)
+            pajak = 0.15;
+            else 
+            pajak = 0.2;
+            gaji_bersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.print("Penghasilan bersih : " + gaji_bersih);
+        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+            if (penghasilan <= 2500000)
+            pajak = 0.15;
+            else if (penghasilan <= 3500000)
+            pajak = 0.2;
+            else 
+            pajak = 0.25;
+            gaji_bersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.print("Penghasilan bersih : " + gaji_bersih);
+        } else
+        System.out.println("Masukan kategori Salah");
+
+    }
+}
